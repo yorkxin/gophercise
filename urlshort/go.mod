02fee0d1f07dec09c980082b3de6e7f34997f2ba
Yucheng Chuang <ducksteven@gmail.com>
2020-12-29 16:35:21 +0900

    hack go mod

---
+++ urlshort/go.mod
@@ -0,0 +1,3 @@
+module github.com/yorkxin/Gophercise/urlshort
+
+go 1.15
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gophercises/urlshort"
+	"github.com/yorkxin/Gophercise/urlshort"
 )
 
 func main() {
