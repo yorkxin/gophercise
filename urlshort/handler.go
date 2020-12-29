ef14c5ee9226140fb7c236dd21ce34d0fbdfef36
Yucheng Chuang <ducksteven@gmail.com>
2020-12-29 17:01:45 +0900

    Urlshort: basic implmentation

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,7 +2,14 @@ package urlshort
 
 import (
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
+
+type redirectionEntry struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -11,8 +18,13 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		if target, ok := pathsToUrls[r.URL.Path]; ok == true {
+			http.Redirect(w, r, target, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -32,6 +44,16 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	var list []redirectionEntry
+	if err := yaml.Unmarshal(yml, &list); err != nil {
+		return fallback.ServeHTTP, err
+	}
+
+	laMap := map[string]string{}
+
+	for _, entry := range list {
+		laMap[entry.Path] = entry.URL
+	}
+
+	return MapHandler(laMap, fallback), nil
 }
