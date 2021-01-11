65142b3814544997a8ed32107f3e489ea474ea6c
Yucheng Chuang <ducksteven@gmail.com>
2021-01-11 17:40:17 +0900

    Sitemap: Step 4: Refactoring extractHrefs function

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -21,13 +22,22 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	links, _ := link.Parse(resp.Body)
 	reqURL := resp.Request.URL
 	baseURL := &url.URL{
 		Scheme: reqURL.Scheme,
 		Host:   reqURL.Host,
 	}
 	base := baseURL.String()
+
+	hrefs := extractHrefs(resp.Body, base)
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+}
+
+func extractHrefs(reader io.Reader, base string) []string {
+	links, _ := link.Parse(reader)
 
 	var hrefs []string
 	for _, aLink := range links {
@@ -38,7 +48,5 @@ func main() {
 		}
 	}
 
-	for _, href := range hrefs {
-		fmt.Println(href)
-	}
+	return hrefs
 }
