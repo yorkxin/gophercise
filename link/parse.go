548bf725ce333bcff5bb5343dd5bd9d04f89a3f8
Yucheng Chuang <ducksteven@gmail.com>
2021-01-11 15:16:01 +0900

    Link: Step 2: DFS HTML Tree

--- link/parse.go
+++ link/parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link holds parsed data of <a href="...">text</a>
 type Link struct {
@@ -10,6 +15,24 @@ type Link struct {
 
 // Parse reads an HTML document, then returns links in the doc.
 func Parse(reader io.Reader) ([]Link, error) {
-	// TODO: implementation
+	doc, err := html.Parse(reader)
+
+	if err != nil {
+		return nil, err
+	}
+
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(node *html.Node, padding string) {
+	msg := node.Data
+	if node.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+
+	fmt.Println(padding, msg)
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		dfs(child, padding+"  ")
+	}
 }
