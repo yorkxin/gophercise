4be8ee65d1b570399bfbeb20b64afae736060467
Yucheng Chuang <ducksteven@gmail.com>
2021-01-11 15:53:26 +0900

    Link: Step 6: Read from file

---
+++ link/cmd/run/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/yorkxin/Gophersise/link"
+)
+
+func main() {
+	flag.Parse()
+	filename := flag.Arg(0)
+
+	if filename == "" {
+		fmt.Fprintln(os.Stderr, "First argument must be a filename")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	reader, err := os.Open(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	links, _ := link.Parse(reader)
+
+	for _, link := range links {
+		fmt.Printf("%+v\n", link)
+	}
+}
