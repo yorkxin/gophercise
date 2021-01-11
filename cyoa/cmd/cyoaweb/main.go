6c1768227de8ced49d1a5e8ecc674ba7fb14b62f
Yucheng Chuang <ducksteven@gmail.com>
2021-01-11 14:12:04 +0900

    Cyoa: Step 8: Specify a custom path func

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"text/template"
 
 	"github.com/yorkxin/Gophercise/cyoa"
 )
@@ -26,9 +28,50 @@ func main() {
 		panic(err)
 	}
 
-	// myTemplate := template.Must(template.New("").Parse("Hi {{.Title}}"))
-	handler := cyoa.NewHandler(story)
+	myTemplate := template.Must(template.New("myTemplate").Parse(myHTML))
+
+	handler := cyoa.NewHandler(
+		story,
+		cyoa.WithTemplate(myTemplate),
+		cyoa.WithPathFunction(myPathFunc),
+	)
 	address := fmt.Sprintf("localhost:%d", *port)
 	fmt.Println("Starting server at:", address)
 	log.Fatal(http.ListenAndServe(address, handler))
 }
+
+func myPathFunc(request *http.Request) string {
+	path := strings.Trim(request.URL.Path, " ")
+
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+
+	// "/story/intro" => "intro"
+	return path[len("/story/"):]
+}
+
+var myHTML = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Choose Your Own Adventure!</title>
+</head>
+<body>
+  <h1>{{.Title}}</h1>
+
+  <article>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+  </article>
+
+  <ul>
+    {{range .Options}}
+    <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+  </ul>
+</body>
+</html>`
