5534e5168463c42e70d9ae9a9379deb542fce0c6
Yucheng Chuang <ducksteven@gmail.com>
2021-01-11 20:47:21 +0900

    Sitemap: Step 5: BFS Traverse links

--- sitemap/main.go
+++ sitemap/main.go
@@ -14,10 +14,44 @@ import (
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the URL you want to build sitemap for")
 
-	hrefs := getHrefsFromURL(*urlFlag)
+	hrefs := bfs(*urlFlag, 5)
 	for _, href := range hrefs {
 		fmt.Println(href)
 	}
+}
+
+func bfs(urlToAccess string, maxDepth int) []string {
+	visited := make(map[string]bool) // true = visited; otherwise not visited
+
+	nextVisit := []string{urlToAccess}
+
+	for depth := 0; depth <= maxDepth; depth++ {
+		toVisit := nextVisit
+		nextVisit = make([]string, 0)
+
+		for _, visitURL := range toVisit {
+			fmt.Printf("[D=%d] ", depth)
+
+			if visited[visitURL] == true {
+				fmt.Printf("\x1b[34mskip\x1b[m : %s\n", visitURL)
+				continue
+			}
+			fmt.Printf("\x1b[1;32mvisit\x1b[m: %s\n", visitURL)
+			visited[visitURL] = true
+
+			nextVisit = append(nextVisit, getHrefsFromURL(visitURL)...)
+
+			for _, newURL := range nextVisit {
+				fmt.Printf("   + %s\n", newURL)
+			}
+		}
+	}
+	allVisited := make([]string, 0, len(visited))
+	for visitedURL := range visited {
+		allVisited = append(allVisited, visitedURL)
+	}
+
+	return allVisited
 }
 
 func getHrefsFromURL(urlToAccess string) []string {
